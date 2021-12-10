d0949cc5f9f6a57afdf215516177fd5ccef33993
matvei4iz <91488394+matvei4iz@users.noreply.github.com>
2021-12-10 23:10:27 +0300

    3.1.1

---
+++ src/main/java/com/example/crudspringboot/model/Role.java
@@ -0,0 +1,39 @@
+package com.example.crudspringboot.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
